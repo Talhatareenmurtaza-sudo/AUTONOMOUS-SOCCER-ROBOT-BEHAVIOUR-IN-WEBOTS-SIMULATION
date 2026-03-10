a8ed0431b2023887d831bed261ed1656c1f0d91e
Talhatareenmurtaza-sudo <husnainrajput17@gmail.com>
2026-03-10 14:47:10 +0500

    Add files via upload

---
+++ controllers/soccerproject/main.cpp
@@ -0,0 +1,14 @@
+
+
+#include "Soccer.hpp"
+
+#include <cstdlib>
+
+using namespace webots;
+
+int main(int argc, char **argv) {
+  Soccer *controller = new Soccer();
+  controller->run();
+  delete controller;
+  return EXIT_FAILURE;
+}
